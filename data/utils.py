fbded30a6468238dbf7e2333687416c202b4c768
gwilczynski95 <g.wilczynski95@gmail.com>
2022-01-07 19:39:18 +0100

    Speed up loading files

--- data/utils.py
+++ data/utils.py
@@ -4,14 +4,20 @@ import PIL
 import cv2
 import numpy as np
 from PIL import Image
+import pyvips
 
 
 def load_image(image_path):
-    raw_image = np.array(Image.open(image_path))
-    if len(raw_image.shape) < 3:
-        raw_image = cv2.imread(image_path)
-    elif raw_image.shape[2] != 3:
-        raw_image = np.array(Image.open(image_path).convert("RGB"))
+    image = pyvips.Image.new_from_file(image_path, access='sequential')
+    mem_img = image.write_to_memory()
+    try:
+        raw_image = np.frombuffer(mem_img, dtype=np.uint8).reshape(image.height, image.width, 3)
+    except ValueError:
+        raw_image = np.array(Image.open(image_path))
+        if len(raw_image.shape) < 3:
+            raw_image = cv2.imread(image_path)
+        elif raw_image.shape[2] != 3:
+            raw_image = np.array(Image.open(image_path).convert("RGB"))
     return raw_image
 
 
@@ -20,3 +26,8 @@ def save_image(path: str, data: Any) -> None:
         data = PIL.Image.fromarray(data)
     data.save(path)
     print(f'saved: {path}')  # todo: change it to logging
+
+
+if __name__ == '__main__':
+    test_path = '/home/grzegorz/projects/museum/data/properly_cut_images/mythological-painting/32-Apollo and Marsyas.jpg'
+    load_image(test_path)
